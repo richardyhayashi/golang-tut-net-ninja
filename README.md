d17dc112dd90d77a5e81d49a5c719dd800111143
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-08-06 17:36:56 +0900

    Finished Chapter 4.

--- README.md
+++ README.md
@@ -2,18 +2,23 @@
 
 ## Run Go File
 
+`$ cd src`
 `$ go run main.go`
 
 ## Number Ranges
 
 'https://go.dev/ref/spec#Numeric_types'
 
+## Print Specifiers
+
+`https://pkg.go.dev/fmt`
+
 ## YouTube
 
 1. Introduction & Setup
 2. Your First Go File
 3. Variables, Strings & Numbers
-4.
+4. Printing & Formatting Strings
 5.
 6.
 7.
--- src/main.go
+++ src/main.go
@@ -4,38 +4,27 @@ import "fmt"
 
 // Entry point of app.
 func main() {
-	// Strings
-	// var nameOne string = "Mario"
-	// var nameTwo = "Luigi"
-	// var nameThree string
+	age := 35
+	name := "John"
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// Pint
+	fmt.Print("Hello")
+	fmt.Print(", World!\n")
+	fmt.Print("New line.\n")
 
-	// nameOne = "Peach"
-	// nameThree = "Bowser"
+	// Println
+	fmt.Println("Hello World")
+	fmt.Println("Goodbye World!")
+	fmt.Println("My age is", age, "and my name is", name)
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// Printf (formatted strings) %_ = format specifier
+	fmt.Printf("My age is %v and my name is %v\n", age, name)
+	fmt.Printf("My age is %q and my name is %q\n", age, name)
+	fmt.Printf("Age is of type %T\n", age)
+	fmt.Printf("Your scored %f points!\n", 255.55)
+	fmt.Printf("Your scored %0.1f points!\n", 255.55)
 
-	// // Only in functions.
-	// nameFour := "Yoshi"
-
-	// fmt.Println(nameFour)
-
-	// ints
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	// Bits & memory
-	// int, int8, int16, int32, int64
-	// uint, uint8, uint16, uint32, uint64
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-	// var numThree uint16 = 256
-
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 883759237594363564868547567.7
-	scoreThree := 1.5
+	// Sprintf (Save formatted strings)
+	var str = fmt.Sprintf("My age is %v and my name is %v", age, name)
+	fmt.Println("The saved string is:", str)
 }
